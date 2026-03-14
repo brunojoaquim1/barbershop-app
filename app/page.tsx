55c2b3d9dca01e1625a24a5684cfe1d49b530b4e
Bruno Joaquim <52578899+brunojoaquim1@users.noreply.github.com>
2026-03-14 14:40:02 -0300

    feat: add boking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -11,12 +14,14 @@ export default function Home() {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Joaquim</h2>
         <p>Quinta-Feira, 12 de março</p>
+
         <div className="mt-6 flex items-center gap-1">
           <Input className="my-4" placeholder="Faça sua busca" />
           <Button size="icon" className="bg-primary hover:bg-primary/90">
             <SearchIcon />
           </Button>
         </div>
+
         <div className="relative mt-6 h-[150px] w-full overflow-hidden rounded-xl">
           <Image
             src="/banner-01.svg"
@@ -25,6 +30,30 @@ export default function Home() {
             className="object-cover"
           />
         </div>
+
+        <Card className="mt-6 p-0">
+          <CardContent className="flex items-stretch justify-between p-0">
+            {/* Div da Esquerda */}
+
+            <div className="flex flex-col p-4 px-5">
+              <Badge className="w-fit rounded-xl">Confirmado</Badge>
+              <h3 className="py-2 font-semibold">Corte de Cabelo</h3>{" "}
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia dos Guri</p>
+              </div>
+            </div>
+
+            {/*Div da Direita*/}
+            <div className="flex flex-col items-center justify-center border-l  px-4">
+              <p className="text-sm">Março</p>
+              <p className="text-2xl">13</p>
+              <p className="text-sm">12:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )

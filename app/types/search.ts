1bfbc5cb84e5fdae16c0e4a24eecdbf8008e551d
Bruno Joaquim <52578899+brunojoaquim1@users.noreply.github.com>
2026-03-20 20:53:04 -0300

    feat: add side bar

---
+++ app/types/search.ts
@@ -0,0 +1,26 @@
+export interface QuickSearchOptions {
+  imageSrc: string
+  title: string
+}
+export const QuickSearchItem: QuickSearchOptions[] = [
+  {
+    imageSrc: "/tesoura.svg",
+    title: "Cabelo",
+  },
+  {
+    imageSrc: "/navalha.svg",
+    title: "Barba",
+  },
+  {
+    imageSrc: "/mustache.svg",
+    title: "Acabamento",
+  },
+  {
+    imageSrc: "/footprints.svg",
+    title: "Pezinho",
+  },
+  {
+    imageSrc: "/eye.svg",
+    title: "Sobrancelha",
+  },
+]

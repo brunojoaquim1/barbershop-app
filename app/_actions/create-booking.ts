def163365d39ce4b7dabe862dec1453310332ab0
Bruno Joaquim <52578899+brunojoaquim1@users.noreply.github.com>
2026-05-23 16:18:28 -0300

    feat: Security check added to booking flow: ensured that only the logged-in user can create their own bookings.

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,6 +1,8 @@
 "use server"
 
+import { getServerSession } from "next-auth"
 import { db } from "../_lib/prisma"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingParams {
   serviceId: string
@@ -9,6 +11,18 @@ interface CreateBookingParams {
 }
 
 export const createBooking = async (params: CreateBookingParams) => {
+  const user = await getServerSession(authOptions).then(
+    (session) => session?.user,
+  )
+
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
+
+  if (user.id !== params.userId) {
+    throw new Error("Usuário não autorizado")
+  }
+
   await db.booking.create({
     data: params,
   })
